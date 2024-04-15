8678c700e0069c386584ae71d8d6689aad963302
Abhishek Baraik <158186156+Abhishek-Baraik@users.noreply.github.com>
2024-04-16 03:50:42 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,9 +3,9 @@ import ErrorHandler from './error.js'
 import jwt from 'jsonwebtoken'
 import {User} from '../models/userSchema.js'
 export const isAuthorized = CatchAsyncError(async (req,res,next)=>{
-    const {token} = req.cookies.token;
+    const {token} = req.cookies;
     if(!token){
-        return next(new ErrorHandler("User is not authorized Sir",401))
+        return next(new ErrorHandler("User is not authorized",401))
     }
     const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
     req.user = await User.findById(decoded.id)
