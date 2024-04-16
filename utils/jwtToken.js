3d8334af161b4a7c1a484d08300c04cda5a85c37
Abhishek Baraik <158186156+Abhishek-Baraik@users.noreply.github.com>
2024-04-16 19:49:35 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,7 +3,9 @@ export const sendToken = (user, statusCode, res, message) => {
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    )
+    ),
+    secure: process.env.NODE_ENV === 'production', // Set secure flag based on environment
+    httpOnly: true,
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
